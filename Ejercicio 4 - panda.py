e2d06579433d2c21e2f99af0200e9c5f246bdcc8
Elizabeth Borbon <elyborboon@gmail.com>
2025-02-28 09:47:51 -0600

    Add files via upload

---
+++ Ejercicio 4 - panda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+serie1 = pd.Series([1,2,3,4,5])
+serie2 = pd.Series([9,8,7,6,5])
+print (serie1)
+print (serie2)
+print (serie1 + serie2)
+print (serie1 - serie2)
+print (serie1 * serie2)
+print (serie1 / serie2)
